d0c47e74bfb8b0ff5b17b2fbadaf0001346d1590
이떵춘 <kor1du0312@gmail.com>
2023-10-20 16:08:40 +0900

    프로젝트 초기 세팅

---
+++ backend/src/main/java/com/OAuthPractice/OAuthPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.OAuthPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthPracticeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/OAuthPractice/OAuthPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OAuthPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
